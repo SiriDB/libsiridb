273461d5784100d05bedc926a4ca2a1ee83fb438
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-06-12 17:36:39 +0200

    Removed unused error codes

--- include/errmap.h
+++ include/errmap.h
@@ -11,16 +11,14 @@
 #define ERR_MEM_ALLOC       -1
 #define ERR_SOCK_FD         -2
 #define ERR_SOCK_CONNECT    -3
-#define ERR_PENDING         -4
-#define ERR_THREAD_START    -5
-#define ERR_NO_REPLY        -6
-#define ERR_CANCELLED       -7
-#define ERR_INVALID_STAT    -8
-#define ERR_CORRUPT         -9
-#define ERR_DESTROYED       -10
-#define ERR_OVERWRITTEN     -11
-#define ERR_SOCK_WRITE      -12
-#define ERR_OCCUPIED        -13
+#define ERR_SOCK_WRITE      -4
+#define ERR_PENDING         -5
+#define ERR_CANCELLED       -6
+#define ERR_INVALID_STAT    -7
+#define ERR_CORRUPT         -8
+#define ERR_DESTROYED       -9
+#define ERR_OVERWRITTEN     -10
+#define ERR_OCCUPIED        -11
 #define ERR_UNKNOWN         -100
 
 const char * siridb_strerror(int err);
